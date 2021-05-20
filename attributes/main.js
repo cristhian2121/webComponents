c8265783a90c2e90ce7716e5cc57b1a4aa596a49
maestro_roshi <ccdelgadop@gmail.com>
2021-05-19 23:25:33 -0500

    add slot and attributes for our components

---
+++ attributes/main.js
@@ -0,0 +1,70 @@
+const template = document.createElement('div')
+template.innerHTML = `
+    <style>
+        p {
+            color: blue
+        }
+        .para-two {
+            font-size: 1.1rem;
+            color: red;
+        }
+
+    </style>
+    <p>Hi world</p>
+    <p class="para-two">Other parafs</p>
+`
+
+class MyElement extends HTMLElement {
+
+    
+    constructor(){
+        super() // access to methods in html elements
+        // add Shadow DOM
+        this.attachShadow({
+            mode: 'open' // is important thar it's open because
+            // so we can see into component
+        })
+        this.title = this.getAttribute('title')
+        this.img = this.getAttribute('image')
+    }
+
+
+    getTemplate(){
+        const template = document.createElement('template')
+        template.innerHTML = `
+            <section>
+                <h2>
+                    ${this.title || ''}
+                </h2>
+                <div>
+                    <img src=${this.img || ''} />
+                </div>
+
+            </section>
+            ${this.getStyles()}
+        `
+        return template
+    }
+
+    getStyles = () => `
+        <style>
+            h2 {
+                color: green;
+            }
+        </style>
+    `
+
+    render() {
+        // we used shadowRoot for create a new Dom into principal DOM
+        this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true))
+    }
+
+    connectedCallback() {
+        this.render()
+    }
+
+
+
+}
+
+customElements.define("my-element", MyElement)
