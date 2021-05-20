e26cc6e726ab1ebfd8af8e382debb0c7dd0808bd
maestro_roshi <ccdelgadop@gmail.com>
2021-05-19 22:46:24 -0500

    I used template label with Js for content shadow an profesional but the style are a problem

---
+++ template/main.js
@@ -0,0 +1,58 @@
+const template = document.createElement('div')
+template.innerHTML = `
+    <style>
+        p {
+            color: blue
+        }
+        .para-two {
+            font-size: 1.1rem;
+            color: red;
+        }
+
+    </style>
+    <p>Hi world</p>
+    <p class="para-two">Other parafs</p>
+`
+
+class MyElement extends HTMLElement {
+    constructor(){
+        super() // access to methods in html elements
+    }
+
+    getTemplate(){
+        const template = document.createElement('template')
+        template.innerHTML = `
+            <section>
+                <h2 class="title">I'm a H2</h2>
+                <div>
+                    <p>I am more that a element</p>
+                </div>
+            </section>
+            ${this.getStyles()}
+        `
+        return template
+    }
+
+    getStyles = () => `
+        <style>
+            h2 {
+                color: green;
+            }
+        </style>
+    `
+
+    render() {
+        // clone elemnt to template is true beacuse is recursive
+        // clone all node chield
+        this.appendChild(this.getTemplate().content.cloneNode(true))
+    }
+
+    connectedCallback() {
+        this.render()
+    }
+
+
+
+}
+
+customElements.define("my-element", MyElement)
